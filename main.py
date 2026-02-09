60a11ac05b970e838603dba82c0c405af4078138
Aqsa <codewithaqsa@gmail.com>
2026-02-09 18:00:01 +0500

    plot the fake data dynamically

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-figure = px.line()
+def get_data(days):
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures = [10, 11, 15]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
 
-st.plotly_chart()
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature"})
+
+st.plotly_chart(figure)
+
